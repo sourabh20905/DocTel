03267ed8dabbcef8995584b0873f1a87c4176def
unknown <sourabh20905@gmail.com>
2023-07-01 22:34:03 +0530

    initial commit

---
+++ src/Components/FooterComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <div className="container" style={{ bottom: 0 }}>
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>© Copyright @2023 Sourabh Sharma</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
